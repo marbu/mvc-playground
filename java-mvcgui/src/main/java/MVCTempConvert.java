6306c236a9f5a5fff85e1ae03633521e56d2f695
Martin Bukatovic <martin.bukatovic@gmail.com>
2017-03-05 23:55:49 +0100

    java-mvcgui: One More Observer
    based on http://csis.pace.edu/~bergin/mvc/mvcgui.html

--- java-mvcgui/src/main/java/MVCTempConvert.java
+++ java-mvcgui/src/main/java/MVCTempConvert.java
@@ -6,5 +6,6 @@ public class MVCTempConvert
 		new FarenheitGUI(temperature, 100, 100);
 		new CelsiusGUI(temperature, 100, 250);
 		new GraphGUI(temperature, 100, 400);
+		new SliderGUI(temperature, 100, 550);
 	}
 }
