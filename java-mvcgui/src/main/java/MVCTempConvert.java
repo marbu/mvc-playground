540b9123f0d21ea23192a60e330b275bb3a5e052
Martin Bukatovic <martin.bukatovic@gmail.com>
2017-03-05 20:23:23 +0100

    java-mvcgui: Graphical GUI extension
    based on http://csis.pace.edu/~bergin/mvc/mvcgui.html

--- java-mvcgui/src/main/java/MVCTempConvert.java
+++ java-mvcgui/src/main/java/MVCTempConvert.java
@@ -5,5 +5,6 @@ public class MVCTempConvert
 		TemperatureModel temperature = new TemperatureModel();
 		new FarenheitGUI(temperature, 100, 100);
 		new CelsiusGUI(temperature, 100, 250);
+		new GraphGUI(temperature, 100, 400);
 	}
 }
